8681abdef8635449561bf82478b43deac974b34e
Dirk Bartley <dbartley@spotx.tv>
2018-01-24 15:27:34 -0700

    passing the public key from the client to the server, and lots of edits in Keypair class

--- admin_api/crypt.py
+++ admin_api/crypt.py
@@ -11,28 +11,41 @@ from Crypto import Random
 from Crypto.PublicKey import RSA
 from Crypto import Random
 
-exepath = os.path.dirname(os.path.realpath(__file__))
-cnfgfile = '%s/keys.cfg' % exepath
-# print "config file is %s" % (cnfgfile)
-config = ApiParser()
-config.read(cnfgfile)
-
 # pylint: disable=
 
 
-class Keypair(RSA._RSAobj):
+class Keypair(object):
     """Lets use public and private keys."""
 
-    def __init__(self, keyname='mykeys'):
+    def __init__(self, cnfgfile=None, keyname='mykeys', pubkeystring=None):
         """Key Pair property initialize."""
         self.debuggenkey = False
-        self.keypairname = 'mykeys'
+        self.priv_key_sting = None
+        self.priv_key_object = None
+        if keyname:
+            self.keypairname = keyname
+        else:
+            self.keypairname = 'mykeys'
 
-        # get the public key string from the config file
-        self.public_key_string = config.safe_get(self.keypairname, 'public')
-        self.priv_key_sting = config.safe_get('mykeys', 'private')
-        if not self.priv_key_sting:
-            print('Private key Not in config will generate')
+        if cnfgfile:
+            self.cnfgfile = cnfgfile
+        else:
+            oneup = os.path.abspath(os.path.join(__file__, ".."))
+            self.cnfgfile = '%s/keys.cfg' % oneup
+            print "config file is %s" % (self.cnfgfile)
+        self.config = ApiParser()
+        self.config.read(self.cnfgfile)
+
+        if not pubkeystring:
+            # get the public key string from the config file
+            self.public_key_string = self.config.safe_get(self.keypairname,
+                                                          'public')
+            self.priv_key_sting = self.config.safe_get(self.keypairname,
+                                                       'private')
+            if not self.priv_key_sting:
+                print('Private key Not in config will generate')
+        else:
+            self.public_key_string = pubkeystring
 
         if self.public_key_string:
             self.__importkeys()
@@ -41,28 +54,30 @@ class Keypair(RSA._RSAobj):
 
     def __importkeys(self):
         if self.priv_key_sting:
-            self.private_key_object = RSA.importKey(self.priv_key_sting)
+            self.priv_key_object = RSA.importKey(self.priv_key_sting)
         self.public_key_object = RSA.importKey(self.public_key_string)
 
     def __genkeypair(self):
         # generate the key pair and write to config file
         random_generator = Random.new().read
-        self.private_key_object = RSA.generate(4096, random_generator)
-        print 'key is %s' % (self.private_key_object)
-        self.public_key_object = self.private_key_object.publickey()
+        self.priv_key_object = RSA.generate(4096, random_generator)
+        # print 'key is %s' % (self.priv_key_object)
+        self.public_key_object = self.priv_key_object.publickey()
         self.public_key_string = self.public_key_object.exportKey('PEM')
-        config.set('mykeys', 'public', self.public_key_string)
-        self.priv_key_sting = self.private_key_object.exportKey('PEM')
-        config.set('mykeys', 'private', self.priv_key_sting)
+        if self.keypairname not in self.config.sections():
+            self.config.add_section(self.keypairname)
+        self.config.set(self.keypairname, 'public', self.public_key_string)
+        self.priv_key_sting = self.priv_key_object.exportKey('PEM')
+        self.config.set(self.keypairname, 'private', self.priv_key_sting)
 
-        configfile_fv = open(cnfgfile, 'w')
-        config.write(configfile_fv)
+        configfile_fv = open(self.cnfgfile, 'w')
+        self.config.write(configfile_fv)
         print "public and private key written"
 
         if self.debuggenkey:
-            print "can encrypt %s" % self.private_key_object.can_encrypt()
-            print "can sign %s" % self.private_key_object.can_sign()
-            print "has private %s" % self.private_key_object.has_private()
+            print "can encrypt %s" % self.priv_key_object.can_encrypt()
+            print "can sign %s" % self.priv_key_object.can_sign()
+            print "has private %s" % self.priv_key_object.has_private()
 
             print 'public key is %s' % self.public_key_string
 
@@ -79,8 +94,12 @@ class Keypair(RSA._RSAobj):
     def decrypt(self, enc_data):
         """Decrypt a sting."""
         if self.priv_key_sting:
-            return self.private_key_object.decrypt(enc_data)
+            return self.priv_key_object.decrypt(enc_data)
         return None
+
+    def get_pub_key(self):
+        """Return The public key."""
+        return self.public_key_string
 
 
 # print dir(RSA)
