bd9bffa54afe0887672ea2c3133a66a371d0f202
Dirk Bartley <dbartley@spotx.tv>
2018-04-21 14:38:12 -0600

    Add domainmetadata

--- admin_api/pdnsmodels.py
+++ admin_api/pdnsmodels.py
@@ -4,7 +4,7 @@ import os
 from admin_api import ApiParser
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, INTEGER, VARCHAR, SmallInteger
+from sqlalchemy import create_engine, Column, INTEGER, VARCHAR, SmallInteger, Text
 from sqlalchemy.orm import sessionmaker
 
 oneup = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -78,3 +78,70 @@ class Records(Base):
             if not (column.primary_key or column.unique):
                 arguments[name] = getattr(self, name)
         return self.__class__(**arguments)
+
+
+class Domainmetadata(Base):
+    """Model for the Domain meta data."""
+
+    __tablename__ = 'domainmetadata'
+    id = Column(INTEGER(), primary_key=True, nullable=False)
+    domain_id = Column(INTEGER())
+    kind = Column(VARCHAR(length=32))
+    content = Column(Text())
+
+    def __init__(self, domain_id=None, kind=None, content=None):
+        """Initialize properties (sql table columns)"""
+        self.domain_id = domain_id
+        self.kind = kind
+        self.content = content
+
+
+def domqry(name=None, dofirst=False):
+    """get a model for a domain."""
+    qry = session.query(Domains)
+    if name:
+        qry = qry.filter(Domains.name == name)
+    if dofirst:
+        qry = qry.first()
+    return qry
+
+
+# pylint: disable=R0913
+def record_query(domid, type_=None, namelike=None, doall=False, dofirst=False, nottype=None):
+    """Build query on records table."""
+    nsq = session.query(Records)\
+                 .filter(Records.domain_id == domid)
+    if type_:
+        nsq = nsq.filter(Records.type == type_)
+    if nottype:
+        nsq = nsq.filter(Records.type != nottype)
+    if namelike:
+        nsq = nsq.filter(Records.name.like("%%.%s" % (newdom.name)))
+    if dofirst:
+        nsq = nsq.first()
+    elif doall:
+        nsq = nsq.all()
+    return nsq
+
+
+def dupnsrecs(qry, newdom):
+    """Duplicate the NS records of pop to new domain."""
+    for nsrec in qry:
+        print("duplicating NS record %s for %s" % (nsrec, newdom.id))
+        newnsrec = nsrec.duplicate()
+        newnsrec.domain_id = newdom.id
+        newnsrec.name = newdom.name
+        session.add(newnsrec)
+    session.commit()
+
+
+def dupsoarec(mdl, newdomid, newdomain_name):
+    """Duplicate the SOA record from pop to new domain."""
+    print("duplicating %s for %s, with name %s" % (mdl, newdomid, newdomain_name))
+    newsoarec = mdl.duplicate()
+    newsoarec.domain_id = newdomid
+    newsoarec.name = newdomain_name
+    print("newsoarec content %s" % (newsoarec.content))
+    print(newsoarec)
+    session.add(newsoarec)
+    session.commit()
